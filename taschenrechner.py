51504749b98b05d58cda1b1aa4dd8d4e40782bc7
profguetler <gua@htl-leoben.at>
2018-03-12 15:15:44 +0100

    taschenrechner

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,1 +1,15 @@
 # taschenrechner
+'''
+Aufgabe:
+Programmiert einen Taschenrechner mit Python
+
+Eingabe:
+zahl1 (z.B. 5)
+zahl2 (z.B. 4)
+aktion (+,-,*,:) (z.B. *)
+
+Ausgabe:
+z.B: 5 * 4 = 20
+'''
+zahl1 = input("bitte die erste zahl eingeben")
+zahl2 = input("bitte die zweite zahl eingeben")
